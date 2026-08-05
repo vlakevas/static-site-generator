1706e1900fb62fdf77b8a64968d54e7fe5de7de1
vlakevas <gearspeed64@gmail.com>
2026-08-05 17:24:45 +0300

    Convert Text to Html, and more tests

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,8 +22,37 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("This is a text node ", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node1,node2)
+    
 
-
+class TestConvertTextHtml(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+    def test_link(self):
+        node = TextNode("Click me", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props, {"href": "https://boot.dev"})
+    def test_image(self):
+        node = TextNode("A cool picture", TextType.IMAGE, "https://boot.dev/img.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "https://boot.dev/img.png", "alt": "A cool picture"}
+        )    
+    def test_link_missing_url_raises(self):
+        node = TextNode("Click me", TextType.LINK)  # no url provided
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -21,3 +22,29 @@ class TextNode:
 
     def __repr__(self) -> str:
         return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None,text_node.text)
+
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b",text_node.text)
+
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i",text_node.text)
+
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code",text_node.text)
+
+    elif text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("Expected url")
+        return LeafNode("a",text_node.text,{"href":text_node.url})
+
+    elif text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("Expected src")
+        return LeafNode("img","",{"src":text_node.url,"alt":text_node.text})
+
+    raise ValueError(f"{text_node} has an invalid type")
+    
